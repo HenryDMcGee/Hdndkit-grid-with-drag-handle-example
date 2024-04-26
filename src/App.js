c9dfdb7f8ae32e114f5cdcc2c471ad0f897971f6
HenryDMcGee <36491845+HenryDMcGee@users.noreply.github.com>
2024-04-26 16:31:59 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Box } from "grommet";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragOverlay
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  rectSortingStrategy
+} from "@dnd-kit/sortable";
+
+import SortableItem from "./SortableItem";
+
+const App = () => {
+  const [activeId, setActiveId] = useState(null);
+  const [items, setItems] = useState([
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29"
+  ]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  );
+
+  const handleDragStart = (event) => {
+    setActiveId(event.active.id);
+  };
+
+  const handleDragEnd = (event) => {
+    setActiveId(null);
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+      onDragStart={handleDragStart}
+    >
+      <Box
+        flex={true}
+        wrap={true}
+        direction="row"
+        style={{ maxWidth: "600px" }}
+      >
+        <SortableContext items={items} strategy={rectSortingStrategy}>
+          {items.map((id) => (
+            <SortableItem key={id} id={id} handle={true} value={id} />
+          ))}
+          <DragOverlay>
+            {activeId ? (
+              <div
+                style={{
+                  width: "100px",
+                  height: "100px",
+                  backgroundColor: "red"
+                }}
+              ></div>
+            ) : null}
+          </DragOverlay>
+        </SortableContext>
+      </Box>
+    </DndContext>
+  );
+};
+
+export default App;
